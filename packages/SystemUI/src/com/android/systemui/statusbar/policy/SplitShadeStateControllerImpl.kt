445914572e01ea572989b5878f250f5986eb743f
Ghosuto <clash.raja10@gmail.com>
2026-05-23 15:51:06 +0000

    SystemUI: Disable split notification shade when legacy QS split shade is enabled
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SplitShadeStateControllerImpl.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SplitShadeStateControllerImpl.kt
@@ -15,8 +15,11 @@
  */
 package com.android.systemui.statusbar.policy
 
+import android.content.Context
 import android.content.res.Resources
+import android.provider.Settings
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Application
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.flags.Flags
 import com.android.systemui.res.R
@@ -27,8 +30,10 @@ import javax.inject.Inject
  * screen width, and flags.
  */
 @SysUISingleton
-class SplitShadeStateControllerImpl @Inject constructor(private val featureFlags: FeatureFlags) :
-    SplitShadeStateController {
+class SplitShadeStateControllerImpl @Inject constructor(
+    private val featureFlags: FeatureFlags,
+    @Application private val context: Context
+) : SplitShadeStateController {
 
     @Deprecated(
         message = "This is deprecated, please use ShadeInteractor#isSplitShade instead",
@@ -39,6 +44,17 @@ class SplitShadeStateControllerImpl @Inject constructor(private val featureFlags
             ),
     )
     override fun shouldUseSplitNotificationShade(resources: Resources): Boolean {
+        val qsSplitShadeEnabled = Settings.System.getIntForUser(
+            context.contentResolver,
+            Settings.System.QS_SPLIT_SHADE,
+            0,
+            android.os.UserHandle.USER_CURRENT
+        ) != 0
+
+        if (qsSplitShadeEnabled) {
+            return false
+        }
+
         return (resources.getBoolean(R.bool.config_use_split_notification_shade) ||
             (featureFlags.isEnabled(Flags.LOCKSCREEN_ENABLE_LANDSCAPE) &&
                 resources.getBoolean(R.bool.force_config_use_split_notification_shade)))
